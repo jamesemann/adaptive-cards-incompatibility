a26575b85247cbf08264afc89b9597ae77044bf0
James Mann <james.mann@bcs.org>
2023-07-13 23:59:58 +0100

    chore: initial repro

---
+++ Repro/Program.cs
@@ -0,0 +1,24 @@
+﻿using AdaptiveCards.Templating;
+
+namespace Repro;
+
+public class DoesItWork
+{
+    public static async Task Main()
+    {
+        var template = new AdaptiveCardTemplate(@"{
+            ""type"": ""AdaptiveCard"",
+    ""$schema"": ""http://adaptivecards.io/schemas/adaptive-card.json"",
+    ""version"": ""1.5"",
+    ""body"": [
+        {
+            ""type"": ""TextBlock"",
+            ""text"": ""this doesnt work"",
+            ""wrap"": true
+        }
+        ]
+        }");
+            
+        var card = template.Expand(new {});
+    }
+} 
